4667391ddf3873db94daa0fc92205433fb54e5eb
Ali khan <108575988+ALI-KHAN-24@users.noreply.github.com>
2023-06-15 19:00:30 +0530

    Add files via upload

---
+++ OIBSIP LEVEL1 TASK_2/script.js
@@ -0,0 +1,10 @@
+function welcome()
+{
+    window.open("https://github.com/ALI-KHAN-24");
+}
+function welcome2(){
+    window.open("https://drive.google.com/file/d/1-AFwnbqjxQjmtFLQcfAJhaF6SF1iTn_R/view?usp=drive_link");
+}
+function welcome3(){
+    window.open("https://www.linkedin.com/in/ali-khan-18a318208?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmEPWTPtPRzKgySKVqxkDpA%3D%3D");
+}
